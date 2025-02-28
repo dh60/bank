a173cbbe0933fdcdcef4e0f1f4715ed0c5c54bc2
dhdhdhdhdhdhdhdhdhdhdhdhdh <d.h@unb.ca>
2025-02-28 11:36:25 -0400

    Fixed compilation issues with both Drivers

---
+++ ReadDriver.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+
+public class ReadDriver {
+  public static void main(String[] args) {
+    Account readAccount = null;
+    
+    // Read the serialized object
+    try {
+      FileInputStream fileIn = new FileInputStream("testAccount.ser");
+      ObjectInputStream objIn = new ObjectInputStream(fileIn);
+      readAccount = (Account) objIn.readObject();
+    }
+    catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
+    catch (ClassNotFoundException cnfe) {
+      cnfe.printStackTrace();
+    }
+    
+    System.out.println(readAccount.getBalance());
+  }
+}
