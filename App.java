736cc52a1fda9fd13c85258701ec01e77af263e2
dhdhdhdhdhdhdhdhdhdhdhdhdh <d.h@unb.ca>
2025-03-02 16:15:55 -0400

    App has functioning registration.

--- App.java
+++ App.java
@@ -28,13 +28,47 @@ public class App extends Application {
     primaryStage.setScene(scene);
     primaryStage.show();
     
-    // Creating a dummy account for testing purposes.
-    User testUser = new User("dh", "greatpassword", "Daniel Hogarth", "d.h@unb.ca", "555-555-5555", "265 Regent St.");
+    // Creating a dummy Bank for testing purposes.
+    Bank bank = new Bank();
     
     // EventHandler for the loginButton. Sends to the User authenticate method, sets status to True or False.
-    loginButton.setOnAction(e -> {
-      boolean loginResult = testUser.authenticate(usernameField.getText(), passwordField.getText());
-      statusLabel.setText(String.valueOf(loginResult));
+    //loginButton.setOnAction(e -> {
+    //  boolean loginResult = testUser.authenticate(usernameField.getText(), passwordField.getText());
+    //  statusLabel.setText(String.valueOf(loginResult));
+    //});
+    
+    // EventHandler for the Registration. CREATES A NEW STAGE.
+    registerButton.setOnAction(e -> {
+      Stage registerStage = new Stage();
+      registerStage.setTitle("User Registration");
+      
+      // Create UI Elements
+      Label registerUsernameLabel = new Label("Username:");
+      TextField registerUsernameField = new TextField();
+      Label registerPasswordLabel = new Label("Password:");
+      TextField registerPasswordField = new TextField();     
+      Label registerNameLabel = new Label("Name:");
+      TextField registerNameField = new TextField();
+      Label registerEmailLabel = new Label("Email:");
+      TextField registerEmailField = new TextField();
+      Label registerPhoneLabel = new Label("Phone:");
+      TextField registerPhoneField = new TextField();
+      Label registerAddressLabel = new Label("Address:");
+      TextField registerAddressField = new TextField();
+      Button submitButton = new Button("Submit");
+      
+      submitButton.setOnAction(event -> {
+        User newUser = new User(
+          registerUsernameField.getText(),
+          registerPasswordField.getText(),
+          registerNameField.getText(),
+          registerEmailField.getText(),
+          registerPhoneField.getText(),
+          registerAddressField.getText()
+        );
+        bank.addUser(newUser);
+        registerStage.close();
+      });
     });
   }
 }
