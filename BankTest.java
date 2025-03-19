36ca559abdc755fda5f602639feeb61201319f66
ADMSI14 <mohnishdev.budhna@unb.ca>
2025-03-19 07:59:35 -0300

    Added duplicate user test case method

--- BankTest.java
+++ BankTest.java
@@ -3,10 +3,10 @@ public class BankTest {
         testAddUserSuccessfully();
         testAddUserNull();
         testAddDuplicateUser();
-        testAuthenticateSuccess();
-        testAuthenticateFailWrongPassword();
-        testAuthenticateNonExistentUser();
-        testAuthenticateWithNullValues();
+       // testAuthenticateSuccess();
+       // testAuthenticateFailWrongPassword();
+       // testAuthenticateNonExistentUser();
+       // testAuthenticateWithNullValues();
         System.out.println("All tests executed.");
     }
 	private static void testAddUserSuccessfully() {
@@ -26,5 +26,19 @@ public class BankTest {
             assert e.getMessage().equals("User cannot be null") : "Test failed: Incorrect exception message";
         }
     }
+	
+	    private static void testAddDuplicateUser() {
+        try {
+            Bank bank = new Bank();
+            User mockUser = new User("abcde", "123456", "abcde",
+              "abcde@gmail.com", "506 1234567", "123 Bd st");
+            bank.addUser(mockUser);
+            bank.addUser(mockUser);
+            assert false : "Test failed: Duplicate user was added";
+        } catch (IllegalArgumentException e) {
+            assert e.getMessage().equals("User already exists") : "Test failed: Incorrect exception message";
+        }
+    }
+
 
 }
