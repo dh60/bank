6530f9a88aa1cbacfb8f8cbc59852552cb7c23d5
v77r2 <v77r2@unb.ca>
2025-03-26 04:58:49 -0300

    added register method to the bank service class

--- BankService.java
+++ BankService.java
@@ -6,8 +6,8 @@ public class BankService {
     public BankService(Bank bank) {
         this.bank = bank;
     }
-	
-	public User login(String username, String password) {
+
+    public User login(String username, String password) {
         for (User user : bank.getUsers()) {
             if (user.getUsername().equals(username) && user.checkPassword(password)) {
                 return user;
@@ -16,4 +16,17 @@ public class BankService {
         return null;
     }
 
+    public void register(String username, String password, String name, String email, String phone, String address) {
+        User newUser = new User(username, password, name, email, phone, address, bank);  // Pass bank
+        bank.getUsers().add(newUser);
+        try {
+            bank.saveToFile("data.ser");
+        } catch (IOException e) {
+            System.out.println("Register failed to save: " + e.getMessage());
+        }
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
 }
