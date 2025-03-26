8ef4a171dde4f9209d32f166e7c9ec9b59a511aa
ADMSI14 <mohnishdev.budhna@unb.ca>
2025-03-26 00:06:20 -0300

    Added withdraw method

--- AccountService.java
+++ AccountService.java
@@ -7,13 +7,26 @@ public class AccountService {
         this.bank = bank;
     }
 	
-	 public void deposit(Account account, double amount) {
+	public void deposit(Account account, double amount) {
         account.deposit(amount);
         try {
             bank.saveToFile("data.ser");
         } catch (IOException e) {
             System.out.println("Deposit failed to save: " + e.getMessage());
         }
+    }
+	
+	public boolean withdraw(Account account, double amount) {
+        double withdrawn = account.withdraw(amount);
+        if (withdrawn > 0) {
+            try {
+                bank.saveToFile("data.ser");
+            } catch (IOException e) {
+                System.out.println("Withdraw failed to save: " + e.getMessage());
+            }
+            return true;
+        }
+        return false;
     }
 
 }
