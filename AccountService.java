023fb1c5ce2e4bad2e61cd7aad5a297b49b40206
ADMSI14 <mohnishdev.budhna@unb.ca>
2025-03-26 00:02:31 -0300

    Added deposit method

--- AccountService.java
+++ AccountService.java
@@ -6,5 +6,14 @@ public class AccountService {
     public AccountService(Bank bank) {
         this.bank = bank;
     }
+	
+	 public void deposit(Account account, double amount) {
+        account.deposit(amount);
+        try {
+            bank.saveToFile("data.ser");
+        } catch (IOException e) {
+            System.out.println("Deposit failed to save: " + e.getMessage());
+        }
+    }
 
 }
